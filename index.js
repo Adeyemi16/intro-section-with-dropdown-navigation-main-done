ebc1af22439145918dc701d0f535338e95ce929f
Adeyemi <110929615+Adeyemi16@users.noreply.github.com>
2023-06-09 00:35:40 +0100

    third

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ svg.addEventListener('click', () => {
     let flex = document.getElementById('flex');
     flex.classList.remove('flex')
     flex.classList.add('flexb')
+    let body = document.getElementById('body');
+    body.classList.add('bodyb')
 })
 
 svgb.addEventListener('click', () => {
     let flex = document.getElementById('flex');
     flex.classList.add('flex')
     flex.classList.remove('flexb')
+    let body = document.getElementById('body');
+    body.classList.remove('bodyb')
 })
